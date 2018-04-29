ef512460a27541abc5c43bf7caea7464b42be226
danhsu <quirkypiggy@gmail.com>
2018-04-28 17:29:54 -0700

    April 28: Time display done

--- proj2/main.c
+++ proj2/main.c
@@ -10,6 +10,20 @@
 #include "avr.h"
 #include "lcd.h"
 
+#define MAXTIMER 60 // for clock, e.g. 1 minute is 60 seconds, then max is 60
+#define MAXHOUR 24 // 24 hours in a day
+
+/*Global Variables Declarations*/
+char buf[17];
+
+int year = 2018;
+int month = 12;
+int day = 31;
+
+int hours = 23;
+int minutes = 58;
+int seconds = 00;
+
 
 int is_pressed(int r, int c)
 {
@@ -18,16 +32,15 @@ int is_pressed(int r, int c)
 	PORTC = 0;
 	
 	//write weak one on column
-	SET_BIT(PORTC,c+4);
+	SET_BIT(PORTC, c+4);
 	
 	//write strong zero on row
-	SET_BIT(DDRC,r);
-	CLR_BIT(PORTC,r);
-	
+	SET_BIT(DDRC, r);
+	CLR_BIT(PORTC, r);
 	
 	
 	//check state of the button
-	if ((GET_BIT(PINC,c+4)))
+	if ((GET_BIT(PINC, c+4)))
 		return 0;
 	else
 		return 1;
@@ -45,104 +58,242 @@ int get_key()
 	}
 	return 0;
 }
-void init()
-{
-}
 
+
+//TEST
 void blink_led(int delay_ms)
 {
 	//test blinking
 	SET_BIT(PORTB,0);		//Turn on LED
-	wait_avr(delay_ms);			//delay
+	wait_avr(delay_ms);		//delay
 	CLR_BIT(PORTB,0);		//Turn off LED
-	wait_avr(delay_ms);			//delay
+	wait_avr(delay_ms);		//delay
+}
+void test_key(int key)
+{
+	sprintf(buf, "%i",  key);
+	pos_lcd(1, 0);
+	puts_lcd2(buf);
+}
+//-------
+
+
+
+// 30 - 4, 6, 9, 11
+// 31 - 1, 3, 5, 7, 8, 10, 12
+// 28, 29 - 2
+
+
+void increment_timer()
+{
+	seconds++;
+
+	if(seconds>=MAXTIMER){
+		minutes++;
+		seconds=0;
+	}
+	if(minutes>=MAXTIMER){
+		hours++;
+		minutes=0;
+	}
+	if(hours>=MAXHOUR){
+		hours=0;
+		minutes=0;
+		seconds=0;
+	}
+	increment_date();
+
+}
+void increment_date()
+{
+	if (hours == 0 && minutes == 0 && seconds == 0)
+	{
+
+		if (day == 31 && month == 1)
+		{
+			month=2;
+			day=1;
+		}
+		else if (day == 28 && month == 2)
+		{	month = 3;
+			day = 1;
+		}
+		else if (day == 31 && month == 3)
+		{	month = 4;
+			day = 1;
+		}
+		else if (day == 30 && month == 4)
+		{	month = 5;
+			day = 1;
+		}
+		else if (day == 31 && month == 5)
+		{	month = 6;
+			day = 1;
+		}
+		else if (day == 30 && month == 6)
+		{	month = 7;
+			day = 1;
+		}
+		else if (day == 31 && month == 7)
+		{	month = 8;
+			day = 1;
+		}
+		else if (day == 31 && month == 8)
+		{	month = 9;
+			day = 1;
+		}
+		else if (day == 30 && month == 9)
+		{	month = 10;
+			day = 1;
+		}
+		else if (day == 31 && month == 10)
+		{	month = 11;
+			day = 1;
+		}
+		else if (day == 30 && month == 11)
+		{	month = 12;
+			day = 1;
+		}
+		else if (day == 31 && month == 12)
+		{	month = 1;
+			day = 1;
+			year++;
+		}
+		else
+			day++;
+	}
+	
+	
+
+}
+void display()
+{
+	while (1)
+	{
+
+		clr_lcd();
+		sprintf(buf, "%02i:%02i:%02i",  hours, minutes, seconds);
+		pos_lcd(1, 4);
+		puts_lcd2(buf);
+		sprintf(buf, "%02i/%02i/%04i",  month, day, year);
+		pos_lcd(0, 3);
+		puts_lcd2(buf);
+		increment_timer();
+		wait_avr(5);
+
+	}
+}
+void input_mode()
+{
+	sprintf(buf, "%s",  "You Type:");
+	pos_lcd(0, 0);
+	puts_lcd2(buf);
+
+
+
 }
 int main(void)
 {
 	// Blink LED Test
-	//SET_BIT(DDRB,0);				//Setting pin 0 for output mode
-	//CLR_BIT(PORTB,0);				//Turn off LED
-	
+	//SET_BIT(DDRB, 0);				//Setting pin 0 for output mode
+	//CLR_BIT(PORTB, 0);				//Turn off LED
+
+
 	// LCD Display
 	ini_lcd();
 
-	int key;
-	char buf[17];
 	ini_avr();
+
+	int key;
+	char inputBuf[17];
+
+	sprintf(buf, "%s",  "Input Year:");
+	pos_lcd(0, 0);
+	puts_lcd2(buf);
+
+
+	//input_mode();
+
+	//display();
+
 
     while (1) 
     {
 		key = get_key();
-		pos_lcd(0, 0);
 		
 		if (key == 0x01) // Key 1
 		{
-			sprintf(buf, "%02i", key);
-			puts_lcd2(buf);
+			test_key(key);
+			sprintf(inputBuf, "%i", key);
+			clr_lcd();
+			pos_lcd(0, 0);
+			puts_lcd2(inputBuf);
 
-			//blink_led(100);			
 		}
 		else if (key == 0x02) // 2
 		{
-			blink_led(200);			
+			test_key(key);
+		
 		}
 		else if (key == 0x03) // 3
 		{
-			blink_led(300);
+				test_key(key);
+
 		}
 		else if (key == 0x04) // Key A
 		{
-			blink_led(100);
+				test_key(key);
 		}
 		else if (key == 0x05) // 4
 		{
-			blink_led(400);
+				test_key(key);
 		}
 		else if (key == 0x06) // 5
 		{
-			blink_led(500);
+				test_key(key);
 		}
 		else if (key == 0x07) // 6
 		{
-			blink_led(600);
+				test_key(key);
 		}
 		else if (key == 0x08) // B
 		{
-			blink_led(100);
+				test_key(key);
 		}
 		else if (key == 0x09) // 7
 		{
-			blink_led(700);
+				test_key(key);
 		}
 		else if (key == 0x0A) // 8
 		{
-			blink_led(800);
+				test_key(key);
 		}
 		else if (key == 0x0B) // 9
 		{
-			blink_led(900);
+				test_key(key);
 		}
 		else if (key == 0x0C) // C
 		{
-			blink_led(100);
+				test_key(key);
 		}
 		else if (key == 0x0D) // Key *
 		{
-			blink_led(200);
+				test_key(key);
 		}
 		else if (key == 0x0E) // 0
 		{
-			blink_led(1000);
+				test_key(key);
 		}
 		else if (key == 0x0F) // #
 		{
-			blink_led(200);
+				test_key(key);
 		}
 		else if (key == 0x10) // D
 		{
-			blink_led(400);
+				test_key(key);
 		}
+
+		
 
     }
 }
-
