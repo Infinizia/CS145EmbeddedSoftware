4bca49e61b9e8a62829704296a85082ea19be5c5
Kevin Nguyen <hnguyen2493@gmail.com>
2018-05-08 17:18:12 -0700

    Added proj3 files

---
+++ proj3/main.c
@@ -0,0 +1,18 @@
+/*
+ * proj3.c
+ *
+ * Created: 5/8/2018 5:12:18 PM
+ * Author : Divergent
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
