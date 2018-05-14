b70df5900d7a878134854278dfe1df02318e2a26
Kevin Nguyen <hnguyen2493@gmail.com>
2018-05-14 10:54:55 -0700

    testing music

--- proj3/main.c
+++ proj3/main.c
@@ -10,7 +10,7 @@
 #include "lcd.h"
 
 struct note {
-	int freq;	//the freqency of a note
+	int freq;	//the frequency of a note
 	int dur;	//the duration of a note	
 };
 
@@ -86,20 +86,30 @@ unsigned get_key_value()
 //Initialize the PORT# for speaker output
 void init_speaker()
 {
+	SET_BIT(DDRA,0);
+	CLR_BIT(PORTA,0);
 	
 }
 //Function to play the note
 void play_note(int freq,int dur)
 {
-	int k;//K?
-	int t;//T?
-	for (int i = 0; i < k; ++i)
-	{
-		//SET PORT_ to 1
-		//wait(T_h) high frequency
-		//SET PORT_ to 0
-		//wait(T_l) low frequency
-		
+	//double wavelength = 1000 / freq;
+	int k = freq * dur;//K?
+	//int t;//T?
+	//for (int i = 0; i < k; ++i)
+	//{
+		//SET_BIT(PORTA,0);	//SET PORT_ to 1
+		//wait_avr(dur);		//wait(T_h) high frequency
+		//CLR_BIT(PORTA,0);	//SET PORT_ to 0
+		//wait_avr(dur);		//wait(T_l) low frequency
+		//
+	//}
+	for(int i = 0; i < (dur); i++)
+		{
+			SET_BIT(PORTA,0);
+			wait_avr(freq);
+			CLR_BIT(PORTA,0);
+			wait_avr(freq);
 	}
 }
 //Function to play the song, where n is the size of song
@@ -114,16 +124,87 @@ void play_music(struct note *song, int n)
 //function to test speaker
 void test_speaker()
 {
+	SET_BIT(PORTA,0);
+	wait_avr(1);
+	CLR_BIT(PORTA,0);
+	wait_avr(1);
+}
+void play_note_volume(int freq, int dur, double ratio, int tempo) {
+	int duration = (dur)/tempo;
+	for(int i = 0; i < duration; i++)
+	{
+		SET_BIT(PORTA,0);
+		wait_avr(freq*(1-ratio));
+		CLR_BIT(PORTA,0);
+		wait_avr(freq*ratio);
+	}
 	
+}
+void play_music_volume(struct note *song,int numNotes, double ratio, int tempo)
+{
+	for (int i = 0;  i < numNotes; i++)
+	{
+		if ((i > 0) && (song[i].freq == song[i-1].freq)) {
+			// int delay = song[i].duration * 0.01;
+			wait_avr(5);
+		}
+		play_note_volume(song[i].freq*2, (song[i].dur), ratio, tempo);
+		
+		
+	}
+}
+void playHotCrossBuns(int tempo, double volumeRatio) {
+	//hot cross buns: bb aa gggg bb aa gggg g g g g a a a a gg bb aa
+	int numNotesCross = 17;
+
+	struct note hot_cross_buns[] = {
+		{101.2391674,493.88}, // b
+		{113.6363636,440}, // a
+		{127.5510204,(392*2)}, // g for 2 seconds
+		{101.2391674,493.88},
+		{113.6363636,440},
+		{127.5510204,(392*2)},
+		{127.5510204,(392/2)},
+		{127.5510204,(392/2)},
+		{127.5510204,(392/2)},
+		{127.5510204,(392/2)},
+		{113.6363636,(440/2)},
+		{113.6363636,(440/2)},
+		{113.6363636,(440/2)},
+		{113.6363636,(440/2)},
+		{101.2391674,493.88},
+		{113.6363636,440},
+		{127.5510204,(392*2)}
+	};
+	
+	play_music_volume(hot_cross_buns, numNotesCross, volumeRatio, tempo);
+}
+
+void play_test()
+{
+	int numOfNotes = 2;
+	struct note test[] = {
+			{440,113},
+			{466,107},
+		};
+		play_music(test,numOfNotes);
 }
 int main(void)
 {
+	
 	//Initialize LCD
     ini_lcd();
-	
-	//Begin rogram
+	init_speaker();
+
+	struct note A = {440, 1};
+	//Begin program
     while (1) 
     {
+		//test_speaker();
+		//play_note(A.freq,A.dur);
+		//play_music(test,4);
+		//playHotCrossBuns(2,0.5);
+		play_test();
     }
 }
 
